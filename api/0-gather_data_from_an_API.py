368d77c9df1a32883a13a50cf97bf2bc39db8cf4
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-04-09 17:57:48 -0500

    Gather data from an API v2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,25 +9,26 @@ import json
 import sys
 
 
-url_user = 'https://jsonplaceholder.typicode.com/users/'
+url_user = "https://jsonplaceholder.typicode.com/users/"
 
 def get_func():
     """ This function gets data from the placeholders API """
     with urllib.request.urlopen(url_user + sys.argv[1]) as f:
         employee_name = json.loads(f.read().decode())
-    with urllib.request.urlopen(url_user + sys.argv[1] + '/todos/') as f:
+    with urllib.request.urlopen(url_user + sys.argv[1] + "/todos/") as f:
         todo_data = json.loads(f.read().decode())
-    
+
     """ Calculate number of completed tasks and total number of tasks """
     number_of_done_task = sum(1 for todo in todo_data if todo["completed"])
     total_number_of_tasks = len(todo_data)
-    
+    employee_name = employee_name["name"]
+
     """ Display progress report """
     print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name['name'], number_of_done_task, total_number_of_tasks))
+          .format(employee_name, number_of_done_task, total_number_of_tasks))
     for todo in todo_data:
-        if todo['completed']:
-            print("\t{} ".format(todo['title']))
+        if todo["completed"]:
+            print("\t{} ".format(todo["title"]))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     get_func()
