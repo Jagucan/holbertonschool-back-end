3a8acfed3ffa5f74edc9593388555caabd1d40f3
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-04-09 10:24:03 -0500

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,23 +13,23 @@ url_user_id = "https://jsonplaceholder.typicode.com/todos?userId="
 
 def get_func():
     """ Get Data """
-    EMPLOYEE_ID = sys.argv[1]
-    TODO_API_URL = url_user_id + "{}" .format(EMPLOYEE_ID)
+    employee_id = sys.argv[1]
+    api_url = url_user_id + "{}" .format(employee_id)
 
-    response = requests.get(TODO_API_URL)
+    response = requests.get(api_url)
     todo_list = response.json()
 
     NUMBER_OF_DONE_TASKS = sum(1 for todo in todo_list if todo["completed"])
     TOTAL_NUMBER_OF_TASKS = len(todo_list)
 
-    EMPLOYEE_API_URL = url_user + "{}" .format(EMPLOYEE_ID)
+    EMPLOYEE_API_URL = url_user + "{}" .format(employee_id)
 
     employee_response = requests.get(EMPLOYEE_API_URL)
     employee = employee_response.json()
-    EMPLOYEE_NAME = employee["name"]
+    employee_name = employee["name"]
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+          .format(employee_name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
     for todo in todo_list:
         if todo["completed"]:
