e13978191052e1a19fb0e3ee8425835b58042334
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-04-09 20:19:39 -0500

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -9,32 +9,34 @@ import json
 import urllib.request
 
 
-url_user = "https://jsonplaceholder.typicode.com/users/"
+url = "https://jsonplaceholder.typicode.com/users/"
 
 
 def get_func():
     """ This function gets data from the placeholders API """
-    with urllib.request.urlopen(url_user) as f:
+    with urllib.request.urlopen(url) as f:
         employee_data = json.loads(f.read().decode())
 
     all_data = {}
-    for employee in employee_data:
-        with urllib.request.urlopen(url_user + str(employee["id"]) + "/todos/") as f:
-            todo_data = json.loads(f.read().decode())
+    for emp in employee_data:
+        with urllib.request.urlopen(url + str(emp["id"]) + "/todos/") as f:
+            t_data = json.loads(f.read().decode())
 
         """ Calculate number of completed tasks and total number of tasks """
-        number_of_done_task = sum(1 for todo in todo_data if todo["completed"])
-        total_number_of_tasks = len(todo_data)
-        employee_name = employee["name"]
-        user_id = employee["id"]
-        username = employee["username"]
-        
-        for todo in todo_data:
+        number_of_done_task = sum(1 for todo in t_data if todo["completed"])
+        total_number_of_tasks = len(t_data)
+        employee_name = emp["name"]
+        user_id = emp["id"]
+        username = emp["username"]
+
+        for todo in t_data:
             task_completed_status = todo["completed"]
             task_title = todo["title"]
 
         """ Export data to JSON format """
-        data = {user_id: [{"username": username, "task": task_title, "completed": task_completed_status} for todo in todo_data]}
+        data = {user_id: [{"username": username, "task": task_title,
+                           "completed": task_completed_status}
+                            for todo in t_data]}
         all_data.update(data)
         with open("todo_all_employees.json", mode="w") as f:
             json.dump(all_data, f)
