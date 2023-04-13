a8b5113d83d59a8a88d355e0429f5c54206a6cfb
Jagucan <jjgc.2499@gmail.com>
2023-04-13 13:01:09 -0500

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -4,7 +4,6 @@
     returns information about list progress
 """
 
-import csv
 import json
 import requests
 import sys
@@ -14,35 +13,29 @@ url_user = "https://jsonplaceholder.typicode.com/users/"
 
 
 def get_func():
-    """Get data from the placeholders API and export to CSV format"""
-    EMPLOYEE_NAME = requests.get(url_user + sys.argv[1]).json()
-    todo_data = requests.get(url_user + sys.argv[1] + "/todos/").json()
+    """Get data from the placeholders API and export to JSON format"""
+    USER_ID = sys.argv[1]
+    EMPLOYEE_DATA = requests.get(url_user + USER_ID).json()
+    todo_data = requests.get(url_user + USER_ID + "/todos/").json()
 
     """Count completed tasks and generate task titles"""
-    NUMBER_OF_DONE_TASKS = 0
+    NUMBER_OF_DONE_TASK = 0
     TASK_TITLE = []
     for todo in todo_data:
         if todo["completed"] is True:
             TASK_TITLE.append(todo["title"])
-            NUMBER_OF_DONE_TASKS += 1
+            NUMBER_OF_DONE_TASK += 1
 
-    """Export data to CSV format"""
-    USER_ID = sys.argv[1]
-    USERNAME = EMPLOYEE_NAME['username']
+    """ Export data to JSON format """
     data = []
+    USERNAME = EMPLOYEE_DATA['username']
     for todo in todo_data:
         TASK_TITLE = todo["title"]
         TASK_COMPLETED_STATUS = "True" if todo["completed"] else "False"
-        data.append([USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
-    with open("{}.csv".format(USER_ID), mode="w") as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerows(data)
-
-    """ Export data to JSON format """
-    data = {USER_ID: [{"task": TASK_TITLE, "completed": TASK_COMPLETED_STATUS,
-                       "username": USERNAME} for todo in todo_data]}
+        data.append({"task": TASK_TITLE, "completed": TASK_COMPLETED_STATUS,
+                     "username": USERNAME})
     with open("{}.json".format(USER_ID), mode="w") as f:
-        json.dump(data, f)
+        json.dump({USER_ID: data}, f)
 
 
 if __name__ == "__main__":
